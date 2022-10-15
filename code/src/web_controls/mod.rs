f0e0378a6b2451852cd0bb1306252dc22fff827e
Zac Pearson <zacfpearson@gmail.com>
2022-10-15 10:33:13 -0500

    touch events

--- code/src/web_controls/mod.rs
+++ code/src/web_controls/mod.rs
@@ -20,6 +20,13 @@ pub fn setup_web_controls() {
     let up_element_temp_4 = document
         .get_element_by_id("up")
         .expect("should have up element");
+    let up_element_temp_5 = document
+        .get_element_by_id("up")
+        .expect("should have up element");
+    let up_element_temp_6 = document
+        .get_element_by_id("up")
+        .expect("should have up element");
+
 
     let up_element = up_element_temp_2
         .dyn_ref::<HtmlElement>()
@@ -35,6 +42,12 @@ pub fn setup_web_controls() {
         .get_element_by_id("right")
         .expect("should have right element");
     let right_element_temp_4 = document
+        .get_element_by_id("right")
+        .expect("should have right element");
+    let right_element_temp_5 = document
+        .get_element_by_id("right")
+        .expect("should have right element");
+    let right_element_temp_6 = document
         .get_element_by_id("right")
         .expect("should have right element");
 
@@ -54,6 +67,12 @@ pub fn setup_web_controls() {
     let down_element_temp_4 = document
         .get_element_by_id("down")
         .expect("should have down element");
+    let down_element_temp_5 = document
+        .get_element_by_id("down")
+        .expect("should have down element");
+    let down_element_temp_6 = document
+        .get_element_by_id("down")
+        .expect("should have down element");
 
     let down_element = down_element_temp_2
         .dyn_ref::<HtmlElement>()
@@ -69,6 +88,12 @@ pub fn setup_web_controls() {
         .get_element_by_id("left")
         .expect("should have left element");
     let left_element_temp_4 = document
+        .get_element_by_id("left")
+        .expect("should have left element");
+    let left_element_temp_5 = document
+        .get_element_by_id("left")
+        .expect("should have left element");
+    let left_element_temp_6 = document
         .get_element_by_id("left")
         .expect("should have left element");
 
@@ -100,6 +125,22 @@ pub fn setup_web_controls() {
         .expect("should have style help");
     });
 
+    let touch_start_up = Closure::<dyn FnMut()>::new(move || {
+        up_element_temp_5
+            .dyn_ref::<HtmlElement>()
+            .expect("should be HtmlElement").style()
+        .set_property("background-color", "#FD7014")
+        .expect("should have style help");
+    });
+
+    let touch_end_up = Closure::<dyn FnMut()>::new(move || {
+        up_element_temp_6
+            .dyn_ref::<HtmlElement>()
+            .expect("should be HtmlElement").style()
+        .set_property("background-color", "transparent")
+        .expect("should have style help");
+    });
+
     let press_right = Closure::<dyn FnMut()>::new(move || {
         right_element_temp
         .dyn_ref::<HtmlElement>()
@@ -118,6 +159,22 @@ pub fn setup_web_controls() {
         right_element_temp_4
         .dyn_ref::<HtmlElement>()
         .expect("should be HtmlElement").style()
+        .set_property("background-color", "transparent")
+        .expect("should have style help");
+    });
+
+    let touch_start_right = Closure::<dyn FnMut()>::new(move || {
+        right_element_temp_5
+            .dyn_ref::<HtmlElement>()
+            .expect("should be HtmlElement").style()
+        .set_property("background-color", "#FD7014")
+        .expect("should have style help");
+    });
+
+    let touch_end_right = Closure::<dyn FnMut()>::new(move || {
+        right_element_temp_6
+            .dyn_ref::<HtmlElement>()
+            .expect("should be HtmlElement").style()
         .set_property("background-color", "transparent")
         .expect("should have style help");
     });
@@ -143,6 +200,21 @@ pub fn setup_web_controls() {
         .set_property("background-color", "transparent")
         .expect("should have style help");
     });
+    let touch_start_down = Closure::<dyn FnMut()>::new(move || {
+        down_element_temp_5
+            .dyn_ref::<HtmlElement>()
+            .expect("should be HtmlElement").style()
+        .set_property("background-color", "#FD7014")
+        .expect("should have style help");
+    });
+
+    let touch_end_down = Closure::<dyn FnMut()>::new(move || {
+        down_element_temp_6
+            .dyn_ref::<HtmlElement>()
+            .expect("should be HtmlElement").style()
+        .set_property("background-color", "transparent")
+        .expect("should have style help");
+    });
     
     let press_left = Closure::<dyn FnMut()>::new(move || {
         left_element_temp
@@ -165,15 +237,30 @@ pub fn setup_web_controls() {
         .set_property("background-color", "transparent")
         .expect("should have style help");
     });
+    let touch_start_left = Closure::<dyn FnMut()>::new(move || {
+        left_element_temp_5
+            .dyn_ref::<HtmlElement>()
+            .expect("should be HtmlElement").style()
+        .set_property("background-color", "#FD7014")
+        .expect("should have style help");
+    });
+
+    let touch_end_left = Closure::<dyn FnMut()>::new(move || {
+        left_element_temp_6
+            .dyn_ref::<HtmlElement>()
+            .expect("should be HtmlElement").style()
+        .set_property("background-color", "transparent")
+        .expect("should have style help");
+    });
 
     up_element
         .set_onmousedown(Some(press_up.as_ref().unchecked_ref()));
-
     up_element
         .set_onmouseup(Some(release_up.as_ref().unchecked_ref()));
     up_element
-
         .set_onmouseleave(Some(out_up.as_ref().unchecked_ref()));
+    up_element.set_ontouchstart(Some(touch_start_up.as_ref().unchecked_ref()));
+    up_element.set_ontouchend(Some(touch_end_up.as_ref().unchecked_ref()));
 
     right_element
         .set_onmousedown(Some(press_right.as_ref().unchecked_ref()));
@@ -183,6 +270,8 @@ pub fn setup_web_controls() {
 
     right_element
         .set_onmouseleave(Some(out_right.as_ref().unchecked_ref()));
+    right_element.set_ontouchstart(Some(touch_start_right.as_ref().unchecked_ref()));
+    right_element.set_ontouchend(Some(touch_end_right.as_ref().unchecked_ref()));
 
     down_element
         .set_onmousedown(Some(press_down.as_ref().unchecked_ref()));
@@ -192,6 +281,8 @@ pub fn setup_web_controls() {
 
     down_element
         .set_onmouseleave(Some(out_down.as_ref().unchecked_ref()));
+    down_element.set_ontouchstart(Some(touch_start_down.as_ref().unchecked_ref()));
+    down_element.set_ontouchend(Some(touch_end_down.as_ref().unchecked_ref()));
 
     left_element
         .set_onmousedown(Some(press_left.as_ref().unchecked_ref()));
@@ -201,22 +292,32 @@ pub fn setup_web_controls() {
 
     left_element
         .set_onmouseleave(Some(out_left.as_ref().unchecked_ref()));
+    left_element.set_ontouchstart(Some(touch_start_left.as_ref().unchecked_ref()));
+    left_element.set_ontouchend(Some(touch_end_left.as_ref().unchecked_ref()));
 
     press_up.forget();
     release_up.forget();
     out_up.forget();
+    touch_start_up.forget();
+    touch_end_up.forget();
 
     press_right.forget();
     release_right.forget();
     out_right.forget();
+    touch_start_right.forget();
+    touch_end_right.forget();
         
     press_down.forget();
     release_down.forget();
     out_down.forget();
+    touch_start_down.forget();
+    touch_end_down.forget();
     
     press_left.forget();
     release_left.forget();
     out_left.forget();
+    touch_start_left.forget();
+    touch_end_left.forget();
 }
 
 #[wasm_bindgen]
